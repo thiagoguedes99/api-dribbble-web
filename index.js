b724e7b540a12039dab99e3658ef022c8f890e8b
Thiago Guedes Barboza (xb187239) <p000tbarboza@prservicos.com.br>
2017-07-04 08:45:39 -0300

    rota all sem barra

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.all('/*', function(req, res) {
+app.all('*', function(req, res) {
     res.status(200).sendFile(path.join(__dirname + 'dist', 'index.html'));
 });
 
