a46236629e569bcf1dcbaaaaf8d83a1f9a156d35
Thiago Guedes Barboza (xb187239) <p000tbarboza@prservicos.com.br>
2017-07-04 08:54:55 -0300

    rota perdido

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ app.all('*', function(req, res) {
     res.status(200).sendFile(path.join(__dirname + 'dist', 'index.html'));
 });
 
+app.all('**', function(req, res) {
+    res.status(200).sendFile(path.join(__dirname + 'dist', 'index.html'));
+});
+
 app.listen(process.env.PORT || 5000);
