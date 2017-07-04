3717d99ec419c4f2d34edef24c66f792dce61c4e
Thiago Guedes Barboza (xb187239) <p000tbarboza@prservicos.com.br>
2017-07-04 08:50:34 -0300

    rota path join

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 app.all('*', function(req, res) {
     res.status(200).sendFile(path.join(__dirname + 'dist', 'index.html'));
